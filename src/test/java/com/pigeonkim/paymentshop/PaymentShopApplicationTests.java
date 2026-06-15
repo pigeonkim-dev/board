7a819db2fed9f1dc6340dd85afec324354a3c5cb
pigeonkim-dev <pigeonkim.dev@gmail.com>
2026-06-15 13:22:50 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/pigeonkim/paymentshop/PaymentShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeonkim.paymentshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
