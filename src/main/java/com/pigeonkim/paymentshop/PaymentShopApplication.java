04c1cc6906d872b98875d3ed96993a2d077c9f0e
pigeonkim-dev <pigeonkim.dev@gmail.com>
2026-06-15 13:22:50 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/pigeonkim/paymentshop/PaymentShopApplication.java
@@ -0,0 +1,13 @@
+package com.pigeonkim.paymentshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentShopApplication.class, args);
+	}
+
+}
